b34560714d5c1a44c4cf8616325ee8455be1b290
hgeffroy <henri.geffroy@gmail.com>
2025-04-09 14:02:24 +0200

    ex04

--- Matrixlib.py
+++ Matrixlib.py
@@ -1,9 +1,13 @@
 from typing import List, Tuple, TypeVar, Generic
+import math
+
 
 # Conditions for T:
-# - Overloaded + operator
-# - Overloaded - operator
-# - Overloaded * operator
+# - Overloaded add operator
+# - Overloaded sub operator
+# - Overloaded mul operator
+# - Overloaded abs
+# - Overload < > = operators
 T = TypeVar('T')
 
 
@@ -13,54 +17,96 @@ class Vector(Generic[T]):
     """
 
     def __init__(self, coordinates: List[T]):
+        if len(coordinates) == 0:
+            raise ValueError("Vector cannot be empty")
+
+        self._type = type(coordinates[0])
         self._size = len(coordinates)
         self._coordinates = coordinates
 
-    def __str__(self):
-        string = f'Vector size: {self._size}\n'f'Coordinates:\n'
+    def __str__(self) -> str:
+        string = ""
         for coord in self._coordinates:
-            string += f'{coord} \n'
+            string += f'{coord}   '
 
         return string
 
-    def __sizeof__(self):
+    def __len__(self) -> int:
         return self._size
 
-    def __add__(self, other):
+    def __add__(self, other) -> 'Vector[T]':
         coord = []
         for c in range(self._size):
-            coord.append(self._coordinates[c] + other.getcoordinates()[c])
+            coord.append(self._coordinates[c] + other.get_coordinates()[c])
         return Vector(coord)
 
-    def __sub__(self, other):
+    def __sub__(self, other) -> 'Vector[T]':
         coord = []
         for c in range(self._size):
-            coord.append(self._coordinates[c] - other.getcoordinates()[c])
+            coord.append(self._coordinates[c] - other.get_coordinates()[c])
         return Vector(coord)
 
-    def __mul__(self, scalar: T):
+    def __mul__(self, scalar: T) -> 'Vector[T]':
         coord = []
         for c in range(self._size):
             coord.append(self._coordinates[c] * scalar)
         return Vector(coord)
 
-    def getcoordinates(self) -> List[T]:
+    def __mul__(self, v: 'Vector[T]') -> float:
+        if self._size != len(v):
+            raise ValueError('Vector does not have the same size')
+
+        res = 0.
+        for i in range(self._size):
+            res += self._coordinates[i] * v.get_coordinates()[i]
+
+        return res
+
+    def __abs__(self) -> float:
+        return self.norm_1()
+
+    def get_coordinates(self) -> List[T]:
         return self._coordinates
+
+    def get_type(self):
+        return self._type
+
+    def norm(self) -> float:
+        results = 0.
+        # CHECK: If T is a vector, this will be the scalar product between c and c
+        for c in self._coordinates:
+            results += c * c
+        return math.sqrt(results)
+
+    def norm_1(self) -> float:
+        results = 0.
+        for c in self._coordinates:
+            results += abs(c)
+
+        return results
+
+    def norm_inf(self) -> float:
+        abslist = [abs(c) for c in self._coordinates]
+        return max(abslist)
 
 
 def linear_combination(u: List[Vector[T]], coefs: List[T]) -> Vector[T]:
-    if len(u) != len(coefs):
-        raise ValueError("Arguments must have the same size")
+    if len(u) != len(coefs) or len(u) == 0:
+        raise ValueError("Arguments must have the same size greater than 0")
 
-    vec = Vector([0] * len(u))
-    for i in range(len(u)):
+    ttype = type(u[0].get_type())
+    vec = Vector([ttype()] * len(u[0]))
+
+    for i in range(len(coefs)):
         vec += (u[i] * coefs[i])
     return vec
 
 
-
-
-
+def linear_interpolation(u: T, v: T, t: float) -> T:
+    diff = v - u
+    diff = diff * t
+    result = u + diff
+    return result
 
 
 
--- main.py
+++ main.py
@@ -29,14 +29,51 @@ def test01():
 
     vec1 = linear_combination([e1, e2, e3], [10., -2., 0.5])
     print(f'{vec1}')
-    # vec2 = linear_combination([v1, v2], [10., -2.])
-    # print(f'{vec2}')
+    vec2 = linear_combination([v1, v2], [10., -2.])
+    print(f'{vec2}')
 
 
+def test02():
+    print("Testing linear interpolation")
+    print("\nFloats: ")
+    print(linear_interpolation(0., 1., 0.))
+    print(linear_interpolation(0., 1., 1.))
+    print(linear_interpolation(0., 1., 0.5))
+    print(linear_interpolation(21., 42., 0.3))
 
+    print("\nVectors: ")
+    v1 = Vector([2., 1.])
+    v2 = Vector([4., 2.])
+    print(linear_interpolation(v1, v2, 0.3))
+
+
+def test03():
+    print("Testing dot product")
+    v1 = Vector([0., 0.])
+    v2 = Vector([1., 1.])
+    v3 = Vector([-1., 6.])
+    v4 = Vector([3., 2.])
+    print(v1 * v2)
+    print(v2 * v2)
+    print(v3 * v4)
+
+def test04():
+    print("Testing norm")
+    v1 = Vector([0., 0., 0.])
+    v2 = Vector([1., 2., 3.])
+    v3 = Vector([-1., 2.])
+    print("v1: " + str(v1))
+    print(f"norm: {v1.norm()} norm_1: {v1.norm_1()} norm_inf: {v1.norm_inf()}")
+    print("v2: " + str(v2))
+    print(f"norm: {v2.norm()} norm_1: {v2.norm_1()} norm_inf: {v2.norm_inf()}")
+    print("v3: " + str(v3))
+    print(f"norm: {v3.norm()} norm_1: {v3.norm_1()} norm_inf: {v3.norm_inf()}")
+
+def test05():
+    pass
 
 def main():
-    test01()
+    test04()
     return
 
 
