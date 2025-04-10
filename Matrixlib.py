5a616e0caf9c9d041406d7bbc14cf4add06e9a9a
hgeffroy <henri.geffroy@gmail.com>
2025-04-10 08:54:26 +0200

    until ex09

--- Matrixlib.py
+++ Matrixlib.py
@@ -7,60 +7,42 @@ import math
 # - Overloaded sub operator
 # - Overloaded mul operator
 # - Overloaded abs
-# - Overload < > = operators
+# - Overload < > = operators for abs values ? (for norm)
 T = TypeVar('T')
 
 
 class Vector(Generic[T]):
     """
-    Class representing a vector of T.
+        Class representing a vector of T.
     """
 
     def __init__(self, coordinates: List[T]):
         if len(coordinates) == 0:
-            raise ValueError("Vector cannot be empty")
+            raise ValueError('Vector cannot be empty')
 
         self._type = type(coordinates[0])
         self._size = len(coordinates)
         self._coordinates = coordinates
 
     def __str__(self) -> str:
-        string = ""
+        string = ''
         for coord in self._coordinates:
             string += f'{coord}   '
 
-        return string
+        return string + '\n'
 
     def __len__(self) -> int:
         return self._size
 
     def __add__(self, other) -> 'Vector[T]':
-        coord = []
-        for c in range(self._size):
-            coord.append(self._coordinates[c] + other.get_coordinates()[c])
-        return Vector(coord)
+        return Vector(
+            [self._coordinates[c] + other.get_coordinates()[c] for c in range(self._size)])
 
     def __sub__(self, other) -> 'Vector[T]':
-        coord = []
-        for c in range(self._size):
-            coord.append(self._coordinates[c] - other.get_coordinates()[c])
-        return Vector(coord)
+        return Vector([self._coordinates[c] - other.get_coordinates()[c] for c in range(self._size)])
 
     def __mul__(self, scalar: T) -> 'Vector[T]':
-        coord = []
-        for c in range(self._size):
-            coord.append(self._coordinates[c] * scalar)
-        return Vector(coord)
-
-    def __mul__(self, v: 'Vector[T]') -> float:
-        if self._size != len(v):
-            raise ValueError('Vector does not have the same size')
-
-        res = 0.
-        for i in range(self._size):
-            res += self._coordinates[i] * v.get_coordinates()[i]
-
-        return res
+        return Vector([self._coordinates[c] * scalar for c in range(self._size)])
 
     def __abs__(self) -> float:
         return self.norm_1()
@@ -70,6 +52,19 @@ class Vector(Generic[T]):
 
     def get_type(self):
         return self._type
+
+    def get_size(self) -> int:
+        return self._size
+
+    def dot(self, v: 'Vector[T]') -> float:
+        if self._size != len(v):
+            raise ValueError('Vector does not have the same size')
+
+        res = 0.
+        for i in range(self._size):
+            res += self._coordinates[i] * v.get_coordinates()[i]
+
+        return res
 
     def norm(self) -> float:
         results = 0.
@@ -86,19 +81,87 @@ class Vector(Generic[T]):
         return results
 
     def norm_inf(self) -> float:
-        abslist = [abs(c) for c in self._coordinates]
-        return max(abslist)
+        return max([abs(c) for c in self._coordinates])
+
+
+class Matrix(Generic[T]):
+    """
+        Class representing a matrix constituted of Vectors
+    """
+
+    def __init__(self, columns: List[Vector[T]]):
+        if not all(len(columns[i]) == len(columns[0]) for i in range(len(columns))):
+            raise ValueError('All vectors must have the same size')
+
+        self._columns = columns
+        self._nbcolumns = len(columns)
+        self._nbrows = len(columns[0])
+        self._type = columns[0].get_type()
+
+    def __str__(self) -> str:
+        string = ''
+        for i in range(self._nbrows):
+            for j in range(self._nbcolumns):
+                string += f'{self._columns[j].get_coordinates()[i]}   '
+            string += '\n'
+        return string
+
+    def __add__(self, other: 'Matrix[T]') -> 'Matrix[T]':
+        if self.get_shape() != other.get_shape():
+            raise ValueError('Matrix cannot be added with different shapes')
+
+        return Matrix([self._columns[i] + other.get_columns()[i] for i in range(self._nbcolumns)])
+
+    def __sub__(self, other: 'Matrix[T]') -> 'Matrix[T]':
+        if self.get_shape() != other.get_shape():
+            raise ValueError('Matrix cannot be added with different shapes')
+
+        return Matrix([self._columns[i] - other.get_columns()[i] for i in range(self._nbcolumns)])
+
+    def __mul__(self, scalar: T) -> 'Matrix[T]':
+        return Matrix([self._columns[i] * scalar for i in range(self._nbcolumns)])
+
+    def get_shape(self) -> Tuple[int, int]:
+        return self._nbrows, self._nbcolumns
+
+    def get_columns(self) -> List[Vector[T]]:
+        return self._columns
+
+    def mul_vec(self, vec: 'Vector[T]') -> 'Vector[T]':
+        if self._nbcolumns != len(vec):
+            raise ValueError('Matrix must have as many columns as vector dimension')
+
+        return linear_combination(self._columns, vec.get_coordinates())
+
+    def mul_mat(self, mat: 'Matrix[T]') -> 'Matrix[T]':
+        if self._nbcolumns != mat.get_shape()[1]:
+            raise ValueError('First matrix must have as many columns as second matrix has rows')
+
+        return Matrix([self.mul_vec(mat.get_columns()[i]) for i in range(mat.get_shape()[1])])
+
+    def trace(self) -> T:
+        if self._nbcolumns != self._nbrows:
+            raise ValueError('Matrix must be square to compute trace')
+
+        result = self._type()
+        for i in range(self._nbcolumns):
+            result += self._columns[i].get_coordinates()[i]
+        return result
+
+    def transpose(self) -> 'Matrix[T]':
+        return Matrix([Vector([self._columns[i].get_coordinates()[j]
+                               for i in range(self._nbcolumns)]) for j in range(self._nbrows)])
 
 
 def linear_combination(u: List[Vector[T]], coefs: List[T]) -> Vector[T]:
     if len(u) != len(coefs) or len(u) == 0:
-        raise ValueError("Arguments must have the same size greater than 0")
+        raise ValueError('Arguments must have the same size greater than 0')
 
-    ttype = type(u[0].get_type())
+    ttype = type(coefs[0])
     vec = Vector([ttype()] * len(u[0]))
 
     for i in range(len(coefs)):
-        vec += (u[i] * coefs[i])
+        vec += u[i] * coefs[i]
     return vec
 
 
@@ -107,6 +170,26 @@ def linear_interpolation(u: T, v: T, t: float) -> T:
     diff = diff * t
     result = u + diff
     return result
+
+
+def angle_cos(u: Vector[T], v: Vector[T]) -> float:
+    if u.get_size() != v.get_size() or u.get_size() == 0:
+        raise ValueError('Arguments must have the same size greater than 0')
+
+    # CHECK: Division induces approx
+    return (u.dot(v))/(u.norm() * v.norm())
+
+
+def cross_product(u: Vector[T], v: Vector[T]) -> Vector[T]:
+    if u.get_size() != 3 or v.get_size() != 3:
+        raise ValueError('Vectors must be three dimensional')
+
+    ucoord = u.get_coordinates()
+    vcoord = v.get_coordinates()
+
+    return Vector([ucoord[1] * vcoord[2] - ucoord[2] * vcoord[1],
+                   ucoord[2] * vcoord[0] - ucoord[0] * vcoord[2],
+                   ucoord[0] * vcoord[1] - ucoord[1] * vcoord[0]])
 
 
 
--- main.py
+++ main.py
@@ -2,25 +2,25 @@ from Matrixlib import *
 
 
 def test00():
-    print("Testing Addition for vector:")
+    print('Testing Addition for vector:')
     a = Vector([1., 2., 3.])
-    print(f"Vector a: \n{str(a)}")
+    print(f'Vector a: \n{str(a)}')
     b = Vector([4., 5., 6.])
-    print(f"Vector b: \n{str(b)}")
+    print(f'Vector b: \n{str(b)}')
     c = a + b
-    print(f"Vector c = a + b: \n{str(c)}")
+    print(f'Vector c = a + b: \n{str(c)}')
 
-    print("Testing Subtraction for vector:")
+    print('Testing Subtraction for vector:')
     c = a - b
-    print(f"Vector c = a - b: \n{str(c)}")
+    print(f'Vector c = a - b: \n{str(c)}')
 
-    print("Testing Scalar Multiplication for vector:")
+    print('Testing Scalar Multiplication for vector:')
     c = a * 3.
-    print(f"Vector c = a * 3: \n{str(c)}")
+    print(f'Vector c = a * 3: \n{str(c)}')
 
 
 def test01():
-    print("Testing linear combination")
+    print('Testing linear combination')
     e1 = Vector([1., 0., 0.])
     e2 = Vector([0., 1., 0.])
     e3 = Vector([0., 0., 1.])
@@ -34,46 +34,133 @@ def test01():
 
 
 def test02():
-    print("Testing linear interpolation")
-    print("\nFloats: ")
+    print('Testing linear interpolation')
+    print('\nFloats: ')
     print(linear_interpolation(0., 1., 0.))
     print(linear_interpolation(0., 1., 1.))
     print(linear_interpolation(0., 1., 0.5))
     print(linear_interpolation(21., 42., 0.3))
 
-    print("\nVectors: ")
+    print('\nVectors: ')
     v1 = Vector([2., 1.])
     v2 = Vector([4., 2.])
     print(linear_interpolation(v1, v2, 0.3))
 
 
 def test03():
-    print("Testing dot product")
+    print('Testing dot product')
     v1 = Vector([0., 0.])
     v2 = Vector([1., 1.])
     v3 = Vector([-1., 6.])
     v4 = Vector([3., 2.])
-    print(v1 * v2)
-    print(v2 * v2)
-    print(v3 * v4)
+    print(v1.dot(v2))
+    print(v2.dot(v2))
+    print(v3.dot(v4))
+
 
 def test04():
-    print("Testing norm")
+    print('Testing norm')
     v1 = Vector([0., 0., 0.])
     v2 = Vector([1., 2., 3.])
     v3 = Vector([-1., 2.])
-    print("v1: " + str(v1))
-    print(f"norm: {v1.norm()} norm_1: {v1.norm_1()} norm_inf: {v1.norm_inf()}")
-    print("v2: " + str(v2))
-    print(f"norm: {v2.norm()} norm_1: {v2.norm_1()} norm_inf: {v2.norm_inf()}")
-    print("v3: " + str(v3))
-    print(f"norm: {v3.norm()} norm_1: {v3.norm_1()} norm_inf: {v3.norm_inf()}")
+    print('v1: ' + str(v1))
+    print(f'norm: {v1.norm()} norm_1: {v1.norm_1()} norm_inf: {v1.norm_inf()}')
+    print('v2: ' + str(v2))
+    print(f'norm: {v2.norm()} norm_1: {v2.norm_1()} norm_inf: {v2.norm_inf()}')
+    print('v3: ' + str(v3))
+    print(f'norm: {v3.norm()} norm_1: {v3.norm_1()} norm_inf: {v3.norm_inf()}')
+
 
 def test05():
-    pass
+    print('Testing cosine of angle between vectors')
+    v1 = Vector([1., 0.])
+    v2 = Vector([0., 1.])
+    v3 = Vector([-1., 1.])
+    v4 = v3 * -1
+    v5 = Vector([2., 1.])
+    v6 = v5 * 2
+    v7 = Vector([1., 2., 3.])
+    v8 = Vector([4., 5., 6.])
+    print(angle_cos(v1, v1))
+    print(angle_cos(v1, v2))
+    print(angle_cos(v3, v4))
+    print(angle_cos(v5, v6))
+    print(angle_cos(v7, v8))
+
+
+def test06():
+    print('Testing cross product')
+    v1 = Vector([0., 0., 1.])
+    v2 = Vector([1., 0., 0.])
+    v3 = Vector([1., 2., 3.])
+    v4 = Vector([4., 5., 6.])
+    v5 = Vector([4., 2., -3.])
+    v6 = Vector([-2., -5., 16.])
+    print(cross_product(v1, v2))
+    print(cross_product(v3, v4))
+    print(cross_product(v5, v6))
+
+
+def test07():
+    print('Testing matrices multiplication')
+    v1 = Vector([4., 2.])
+    m1 = Matrix([Vector([1., 0.]), Vector([0., 1.])])
+    m2 = m1 * 2
+    m3 = Matrix([Vector([2., -2.]), Vector([-2., 2.])])
+    m4 = Matrix([Vector([2., 1.]), Vector([4., 2.])])
+    m5 = Matrix([Vector([3., -5.]), Vector([6., 8.])])
+    print('Matrix * Vector:')
+    print(m1.mul_vec(v1))
+    print(m2.mul_vec(v1))
+    print(m3.mul_vec(v1))
+
+    print('Matrix * Matrix:')
+    print(m1.mul_mat(m1))
+    print(m1.mul_mat(m4))
+    print(m4.mul_mat(m5))
+
+
+def test08():
+    print('Testing matrix trace computation')
+    m1 = Matrix([Vector([1., 0.]), Vector([0., 1.])])
+    m2 = Matrix([Vector([2., -5., 0.]),
+                 Vector([4., 3., 7.]),
+                 Vector([-2., 3., 4.])])
+    m3 = Matrix([Vector([-2., -8., 4.]),
+                 Vector([1., -23., 4.]),
+                 Vector([0., 6., 4.])])
+    print(m1.trace())
+    print(m2.trace())
+    print(m3.trace())
+
+
+def test09():
+    print('Testing matrix transposition')
+    m1 = Matrix([Vector([1., 0.]), Vector([0., 1.])])
+    m2 = Matrix([Vector([2., -5., 0.]),
+                 Vector([4., 3., 7.]),
+                 Vector([-2., 3., 4.])])
+    m3 = Matrix([Vector([-2., -8., 4.]),
+                 Vector([1., -23., 4.])])
+    print(m1)
+    print(m1.transpose())
+    print(m2)
+    print(m2.transpose())
+    print(m3)
+    print(m3.transpose())
+
 
 def main():
-    test04()
+    # test00()
+    # test01()
+    # test02()
+    # test03()
+    # test04()
+    # test05()
+    # test06()
+    # test07()
+    # test08()
+    test09()
     return
 
 
